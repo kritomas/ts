3cbcb3870377b8a1a63fe43345ed15832445ef6d
kritomas <Unknown>
2024-05-14 16:57:25 +0200

    Initial cards.

--- src/card.tsx
+++ src/card.tsx
@@ -1,8 +1,19 @@
-export const Card : React.FC = () =>
+interface CardProps
+{
+	title:string;
+	image_src:string;
+	text:string;
+}
+
+export const Card : React.FC<{CardProps}> = ({title, image_src, text}) =>
 {
     return (
-        <div>
-            
+        <div style={{backgroundColor: "lightblue", padding: "5px", margin: "2px"}}>
+			<p><b>{title}</b></p>
+			<img src={image_src}></img>
+			<p>{text}</p>
         </div>
     )
 }
+
+export default Card;
